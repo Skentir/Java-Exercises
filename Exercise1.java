3d79af7ac7eb6c666ec35cff889b7f72507f54fb
Skentir <31249788+Skentir@users.noreply.github.com>
2019-05-24 12:36:14 +0800

    Added May 24 Java Exercises
    - Loops and Declarations

---
+++ Exercise1.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Exercise1 {
+  public static void main (String[] args) {
+      int i;
+      System.out.println("Using for loop: ");
+      for (i= 1; i <= 5; i++) {
+        System.out.print("Animo!");
+      }
+      System.out.println("\nUsing do-while: ");
+      do {
+        System.out.print("Animo!");
+        i--;
+      } while (i > 1);
+      System.out.println("\nUsing while loop: ");
+      while (i <= 5) {
+        System.out.print("Animo!");
+        i++;
+      }
+      System.out.println();
+      /* int nVal = 8;
+      System.out.print(nVal + 3+ "7" + 3 + 2);
+      System.out.println();
+      System.out.println("2" + 3 + 4 + 5);
+      System.out.println('c');
+      System.out.println('c'+5);
+
+      double dAns;
+      int nAns;
+      char cAns;
+      boolean bAns;
+
+      System.out.println("Kirsten Sison\n11837292\nBSCS");
+      nAns = 6 % 1 + 7 % 3;
+      System.out.println("The answer is " + nAns);
+      nAns = 6 % (1 + 7) % 3;
+      System.out.println("The answer is " + nAns);
+      dAns = 6.0/4 + 3;
+      System.out.println("The answer is " + dAns);
+      nAns = 'K' + 9;
+      System.out.println("The answer is " + nAns);
+      cAns = 'K' + 9;
+      System.out.println("The answer is " + cAns);
+      bAns = 5.8 >= 8.5 || 9 > 2.5 && 6 == 6.01;
+      System.out.println("The answer is " + bAns); */
+
+  }
+}
