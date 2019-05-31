843c519fbc3b459f8b23f3f825589b14e6e285ab
Kirsten S <daniellesison19@gmail.com>
2019-05-31 16:06:35 +0800

    Java Arrays Exercise

--- Exercise4.java
+++ Exercise4.java
@@ -7,28 +7,28 @@ public class Exercise4 {
     	System.out.print("Enter number of students: ");
     	int size = user_input.nextInt();
     	String[] directory = new String[size];
-    	
+
     	// Populate the array
-    	System.out.println("Enter in the format <lastName>, <firstName>, <initial>: ");
-    	for (int i = 0; i < directory.length; i++) 
-    		directory[i] = user_input.nextLine();
-    		
+      user_input.nextLine();
+      System.out.println("Enter in the format <lastName>, <firstName>, <initial>: ");
+    	for (int i = 0; i < directory.length; i++)
+      	directory[i] = user_input.nextLine();
+
     	// Sort the array
  		int indx, indx2; String surname, temp, firstname;
     	for (int n = 0; n < size; n++) {
-    		indx = directory[n].indexOf(','); 
+    		indx = directory[n].indexOf(',');
     		surname = directory[n].substring(0, indx);
-    		
-    		for (int m = n+1; m < size; m++){
+
+    		for (int m = n+1; m < size; m++) {
     			if (surname.compareToIgnoreCase(directory[m]) > 0) {
     				temp = directory[m];
     				directory[m] = directory[n];
     				directory[n] = temp;
     			}
     		}
-    		
     	}
-    	
+
     	// Display the array
     	for (int m = 0; m < size; m++) {
     		indx = directory[m].indexOf(',') + 1;
@@ -36,9 +36,5 @@ public class Exercise4 {
     		firstname = directory[m].substring(indx, indx2);
     		System.out.println(firstname);
     	}
-    		
-    	
-    }
-    
-    
+   }
 }
