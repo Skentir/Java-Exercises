24d23f88b7222e566f05235dc5abc7df38f09683
Kirsten S <daniellesison19@gmail.com>
2019-06-02 03:16:38 +0800

    Encapsulation Exercise

--- Dog.java
+++ Dog.java
@@ -1,5 +1,34 @@
 import java.util.*;
 
 public class Dog {
+  // Attributes
+  private String name;
   private int age;
+  private String gender;
+//  private final String gender;
+  // Constructor
+  public Dog(String name, int age, String gender) {
+    setName(name);
+    this.age = age;
+    setGender(gender);
+  }
+  // Methods
+  public void setName(String new_name) {
+    name = new_name;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setGender(String new_gender) {
+    gender = new_gender;
+  }
+  public String getGender() {
+    return gender;
+  }
+  public void setAge(int new_age) {
+    age = new_age;
+  }
+  public int getAge() {
+    return age;
+  }
 }
--- DogTest2.java
+++ DogTest2.java
@@ -7,19 +7,43 @@ public class DogTest2 {
     Dog[] dog_list = new Dog[3];
 
     /* Populate Dog Array */
-    int i = 0, age; String name;
+    int i = 0, age; String name, gender;
     while (dog_list.length > i) {
-      System.out.printf("Enter information of Dog %d : \n", i);
+      System.out.printf("Enter information of Dog %d : \n", i+1);
       System.out.print("Name: ");
       name = user_input.nextLine();
       System.out.print("Age: ");
       age = user_input.nextInt();
+      user_input.nextLine();
+      System.out.print("Gender: ");
+      gender = user_input.nextLine();
       /* Set name and age of Dog */
-      dog_list.setName(name);
-      dog_list.setAge(age);
+      dog_list[i] = new Dog(name, age, gender);
       i++;
     }
-
-    /* Display Dog Array */
+    System.out.println();
+    Display(dog_list);
+    System.out.println();
+    /* Increase Age of Dogs */
+    i = 0;
+    while (dog_list.length > i) {
+      System.out.printf("Increasing Age of Dog %d ...\n", i+1);
+      dog_list[i].setAge(dog_list[i].getAge()+1);
+      i++;
+    }
+    System.out.println();
+    Display(dog_list);
   }
+
+  public static void Display(Dog dog_list[]) {
+    /* Display Dog Array */
+    int i = 0;
+    while (dog_list.length > i) {
+      System.out.println("Name: " + dog_list[i].getName());
+      System.out.println("Age: " + dog_list[i].getAge());
+      System.out.println("Gender: " + dog_list[i].getGender());
+      i++;
+    }
+  }
+
 }
