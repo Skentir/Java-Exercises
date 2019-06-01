56490807e818bd78067f19906f99edd26b45640f
Kirsten S <daniellesison19@gmail.com>
2019-06-02 02:18:20 +0800

    Encapsulation

--- DogTest2.java
+++ DogTest2.java
@@ -9,12 +9,15 @@ public class DogTest2 {
     /* Populate Dog Array */
     int i = 0, age; String name;
     while (dog_list.length > i) {
-    System.out.printf("Enter information of Dog %d : \n", i);
-    System.out.print("Name: ");
-    name = user_input.nextLine();
-    ystem.out.print("Age: ");
-    age = user_input.nextInt();
-    i++;
+      System.out.printf("Enter information of Dog %d : \n", i);
+      System.out.print("Name: ");
+      name = user_input.nextLine();
+      System.out.print("Age: ");
+      age = user_input.nextInt();
+      /* Set name and age of Dog */
+      dog_list.setName(name);
+      dog_list.setAge(age);
+      i++;
     }
 
     /* Display Dog Array */
