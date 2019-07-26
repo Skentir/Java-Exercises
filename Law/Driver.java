0565c03214bb738cf9bb12d0249f64f7f9c12f0b
Kirsten S <daniellesison19@gmail.com>
2019-07-27 00:38:44 +1000

    Updated Driver

--- Law/Driver.java
+++ Law/Driver.java
@@ -5,14 +5,17 @@ public class Driver
     public static void main(String[] args)
     {
         LawFirm firm = new LawFirm("My Law Firm");
+        private ArrayList<Employee> employee_list = new ArrayList<>();
+        employee_list.add(new Accountant("Kirsten", 2000.0));
+        employee_list.add(new Accountant("Nathan", 1800.0));
+        employee_list.add(new Lawyer("Nick", 3120.0));
+        employee_list.add(new Janitor("Marc", 1500.0));
 
-        firm.hire(new Accountant("Kirsten", 2000.0));
-        firm.hire(new Accountant("Nathan", 1800.0));
-        firm.hire(new Lawyer("Nick", 3120.0));
-        firm.hire(new Janitor("Marc", 1500.0));
+        for (int i=0; i<employee_list.size(); i++)
+          firm.hire(employee_list.get(i));
         firm.createPayroll();
-        
-        firm.fire(new Janitor("Marc", 100.0));
+
+        firm.fire(employee_list.get(3));
         firm.createPayroll();
     }
 }
