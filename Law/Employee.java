bdeaa28b0d26bfd409ffcde2fc29983101ac8937
Kirsten S <daniellesison19@gmail.com>
2019-07-26 14:10:04 +1000

    Added

--- Law/Employee.java
+++ Law/Employee.java
@@ -1,7 +1,32 @@
 public abstract class Employee
 {
-    protected Employee()
+  private double hours;
+    protected Employee() { }
+    public abstract double getBaseSalary();
+    public double getSalary()
     {
-        
+      if (this instanceof Accountant)
+          this.getBaseSalary();
+      else if (this instanceof Janitor)
+          this.getBaseSalary();
+      else if (this instanceof Lawyer)
+          this.getBaseSalary()
+      /* UM WHAT DO I CALL HERE? NOT SURE*/
+    }
+    public double computeTax(double salary)
+    {
+      double temp = salary - 3200;
+      if (temp > 0)
+        return 0.05 * 3200 + 0.1 * temp;
+      else
+        return 0.05 * salary;
+    }
+    public double getHoursWorked()
+    {
+      return hours;
+    }
+    public void setHoursWorked(double hours)
+    {
+      this.hours = hours;
     }
 }
