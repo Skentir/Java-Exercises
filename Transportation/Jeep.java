aa3905bcd5afe4e3a92efc4137203ebf0287770c
Marc Tiburcio <44109710+marcnt@users.noreply.github.com>
2019-07-31 12:12:35 +0800

    Fix formatting and place vehicle constructor

--- Transportation/Jeep.java
+++ Transportation/Jeep.java
@@ -1,18 +1,19 @@
 public class Jeep extends Vehicle
 {
-  public int CAPACITY = 20;
+    public int CAPACITY = 20;
 
-  public Jeep()
-  {
-    super(20);
-  }
-  @Override
-  public double computeFare(double kilometers)
-  {
-    double km = totalDistance - kilometers;
-    if ( km > 4)
-      return km + 28;
-    else
-      return km * 7;
-  } 
+    public Jeep()
+    {
+        super(CAPACITY);
+    }
+
+    @Override
+    public double computeFare(double kilometers)
+    {
+        double km = totalDistance - kilometers;
+        if ( km > 4)
+            return km + 28;
+        else
+            return km * 7;
+    }
 }
--- Transportation/Vehicle.java
+++ Transportation/Vehicle.java
@@ -1,45 +1,50 @@
-import java.ArrayList;
+import java.util.ArrayList;
 
 public abstract class Vehicle
 {
-private int capacity;
-private double totalDistance;
-private ArrayList<Passenger> passengers;
+    private int capacity;
+    private double totalDistance;
+    private ArrayList<Passenger> passengers;
 
-protected Vehicle(int capacity);
-public boolean accept(Passenger passenger)
-{
-  if (this.CAPACITY > passengers.size())
-  {
-    passengers.add(passenger);
-    return true;
-  } else
-    return false;
-}
+    protected Vehicle(int capacity)
+    {
+        this.capacity = capacity;
+        this.passengers = new ArrayList<>();
+    }
 
-public void remove(Passenger passenger)
-{
-  passengers.remove(passenger);
-}
+    public boolean accept(Passenger passenger)
+    {
+        if (passengers.size() < this.capacity)
+        {
+            passengers.add(passenger);
+            return true;
+        }
 
-public int getCount()
-{
-  return passengers.size();
-}
+        return false;
+    }
 
-public int getCapacity()
-{
-  return this.capacity;
-}
+    public void remove(Passenger passenger)
+    {
+        passengers.remove(passenger);
+    }
 
-public double getTotalDistance()
-{
-  return totalDistance;
-}
+    public int getCount()
+    {
+        return passengers.size();
+    }
 
-public double travel()
-{
-  totalDistance++;
-}
+    public int getCapacity()
+    {
+        return this.capacity;
+    }
 
+    public double getTotalDistance()
+    {
+        return totalDistance;
+    }
+
+    public double travel()
+    {
+        totalDistance++;
+    }
 }
