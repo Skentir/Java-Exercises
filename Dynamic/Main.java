d60412722acca3822f6ee84a71a745199c5b971b
Kirsten S <daniellesison19@gmail.com>
2019-10-19 00:01:39 +0800

    Added skyline solution

---
+++ Dynamic/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    private static Solution.Building[] makeBuildings(int... values) {
+        if (values == null)
+            throw new IllegalArgumentException("values is null");
+        if (values.length % 3 != 0)
+            throw new IllegalArgumentException("values should have length divisible by 3");
+
+        Solution.Building[] buildings = new Solution.Building[values.length / 3];
+        for (int i = 0; i < buildings.length; i++) {
+            Solution.Building b = new Solution.Building();
+            b.left = values[i * 3];
+            b.height = values[i * 3 + 1];
+            b.right = values[i * 3 + 2];
+            buildings[i] = b;
+        }
+        return buildings;
+    }
+
+    public static void main(String[] args) {
+        Solution.skyline(makeBuildings(1, 3, 4, 2, 2, 6));
+        System.out.println();
+        Solution.skyline(makeBuildings(1, 4, 4, 8, 8, 12));
+        System.out.println();
+        Solution.skyline(makeBuildings(1, 7, 5, 3, 4, 8, 4, 10, 10, 7, 3, 13));
+        System.out.println();
+        Solution.skyline(makeBuildings(1, 11, 5, 2, 6, 7, 3, 13, 9, 12, 7, 16, 14, 3, 25, 19, 18, 22, 23, 13, 29, 24, 4, 28));
+        System.out.println();
+        Solution.skyline(makeBuildings(1, 10, 2, 3, 6, 6, 4, 8, 12, 5, 10, 8, 7, 9, 10, 11, 5, 14));
+        System.out.println();
+        Solution.skyline(makeBuildings(1, 6, 6, 2, 4, 3, 4, 9, 5, 7, 10, 10, 8, 5, 9, 9, 11, 10));
+        System.out.println();
+        Solution.skyline(makeBuildings(1, 10, 5, 5, 10, 8));
+        System.out.println();
+        Solution.skyline(makeBuildings(1, 10, 5, 5, 12, 8));
+        System.out.println();
+    }
+}
--- Dynamic/Solution.java
+++ Dynamic/Solution.java
@@ -1,44 +1,142 @@
+/*
+Danielle Kirsten T. Sison
+S16 - CSALGCM
+*/
+
 import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.*;
 
 class Solution {
-    public class Building
+
+    public static class Building
     {
         int left;   //  left x-coordinate
         int right;  //  right x-coordinate
         int height;
     }
 
-    public static void Skyline(Building[] B)
+    public static void skyline(Building[] B)
     {
-        HashMap<Integer, Integer> sky = new HashMap<Integer, Integer>();
-        if (B.length == 0)
-          System.out.println('No skyline.');
-        HashMap<Integer, Integer> points = partition(B, 0, B.length -1);
-        for (<Integer,Integer> p: points)
-          sky.put(p);
-    }
-    public static void partition(Building[], int first, int second)
-    {
-      
-    }
-}
+        List<int[]> sky = new ArrayList<int[]>();
 
-public class MainClass
-{
-  public static void main(String[] args)
-  {
-    Solution sol = new Solution();
-    Solution.Building inner = sol.new Building();
-    Solution.Building[] buildings = new Solution.Building[2];
-    Solution.Building a = sol.new Building();
-    a.left = 1;
-    a.right = 5;
-    a.height = 11;
-    Solution.Building b = sol.new Building();
-    b.left = 1;
-    b.right = 5;
-    b.height = 11;
-    buildings[0] = a;
-    buildings[1] = b;
+        if (B.length == 0)
+          System.out.println("No skyline.");
+        List<int[]> points = partition(B, 0, B.length -1);
+
+        for (int[] arr : points)
+          sky.add(arr);
+
+        for (int i = 0; i < sky.size(); i++)
+        {
+          int[] tmp = sky.get(i);
+          for (int j = 0; j < tmp.length; j++)
+              System.out.print(tmp[j] + " ");
+        }
+
+    }
+    public static List<int[]> partition(Building[] B, int first, int second)
+    {
+      if (first == second)
+      {
+          List<int[]> points = new ArrayList<int[]>();
+          points.add(new int[] {B[first].left, B[first].height});
+          points.add(new int[] {B[first].right, 0});
+          return points;
+      }
+      int mid = first + (second - first)/2;
+      List<int[]> lower = partition(B, first, mid);
+      List<int[]> upper = partition(B, mid+1, second);
+
+      return mergePoints(lower, upper);
+    }
+
+    public static List<int[]> mergePoints(List<int[]> lower, List<int[]> upper)
+    {
+          List<int[]> points = new ArrayList<int[]>();
+          int a = 0, b = 0, max1 = 0, max2 = 0;
+
+          while(a < lower.size() || b < upper.size())
+          {
+              if (a == lower.size()) {
+                points.add(upper.get(b++));
+                continue;
+              }
+              if (b == upper.size()) {
+                points.add(lower.get(a++));
+                continue;
+              }
+              int pt = lower.get(a)[0];
+              if (lower.get(a)[0] < upper.get(b)[0]) {
+                max1 = lower.get(a)[1];
+                pt = lower.get(a)[0];
+                a++;
+              }
+              else if (lower.get(a)[0] > upper.get(b)[0]) {
+                max2 = upper.get(b)[1];
+                pt =  upper.get(b)[0];
+                b++;
+              } else {
+                max1 = lower.get(a)[1];
+                max2 = upper.get(b)[1];
+                a++; b++;
+              }
+
+              int height = Math.max(max1, max2);
+
+              if (points.size() == 0 || height != points.get(points.size()-1)[1]) {
+				        points.add(new int[] {pt, height});
+              }
+			}
+        return points;
+    }
+
+    public static void mergesort(int[] A)
+    {
+      if(A.length > 1) {
+            int mid = A.length / 2;
+
+            // Split left part
+            int[] left = new int[mid];
+            for(int i = 0; i < mid; i++) {
+                left[i] = A[i];
+            }
+
+            // Split right part
+            int[] right = new int[A.length - mid];
+            for(int i = mid; i < A.length; i++) {
+                right[i - mid] = A[i];
+            }
+
+            mergesort(left);
+            mergesort(right);
+
+            int i = 0;
+            int j = 0;
+            int k = 0;
+
+            // Merge left and right arrays
+            while(i < left.length && j < right.length) {
+                if(left[i] < right[j]) {
+                    A[k] = left[i];
+                    i++;
+                } else {
+                    A[k] = right[j];
+                    j++;
+                }
+                k++;
+            }
+
+            // Collect remaining elements
+            while(i < left.length) {
+                A[k] = left[i];
+                i++; k++;
+            }
+
+            while(j < right.length) {
+                A[k] = right[j];
+                j++; k++;
+            }
+    }
   }
 }
